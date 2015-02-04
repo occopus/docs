a0718aa6cce439216098e87b498a9ec4cd82f8eb
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-02-04 16:56:59 +0100

    Experimenting; Still trying to fix structural issue

--- sphinx/ext/ext/ibkeys/ibkeys.py
+++ sphinx/ext/ext/ibkeys/ibkeys.py
@@ -35,14 +35,15 @@ def visit_ibkey_node(self, node): self.visit_desc(node)
 def depart_ibkey_node(self, node): self.depart_desc(node)
 class ibkey(addnodes.desc):
     def __init__(self, directive, refkey, key_elem, doc):
-        targetnode = nodes.target('', '', ids=[refkey])
-
         label = nodes.strong('@provides', '@provides')
         sep = nodes.Text(': ', ': ')
-        par = addnodes.desc_signature('', '', label, sep, key_elem)
+        par = addnodes.desc_signature('<autodoc>', '', label, sep, key_elem)
         update_attrs(par, directive)
+        self.name = 'py:ibkey'
 
-        super(ibkey, self).__init__('', targetnode, par, doc)
+        super(ibkey, self).__init__()
+        self += par
+        self += doc
         update_attrs(self, directive)
 
 def visit_iblist_entry_node(self, node): self.visit_desc(node)
@@ -72,6 +73,10 @@ class iblist_entry(addnodes.desc):
 class ibkeylist(addnodes.desc_content):
     pass
 
+class dummy(nodes.Admonition, nodes.Element): pass
+def visit_dummy_node(self, node): self.visit_admonition(node)
+def depart_dummy_node(self, node): self.depart_admonition(node)
+
 class DeclIBKey(Directive):
     has_content = True
 
@@ -87,10 +92,15 @@ class IBKeyListDirective(Directive):
 from sphinx.util.docfields import DocFieldTransformer
 #from sphinx.directives import ObjectDescription
 from sphinx.domains.python import PyObject
+from sphinx.util.compat import make_admonition
 
 class IBKeyDirective(PyObject):
     has_content = True
     doc_field_types = PyObject.doc_field_types
+    required_arguments = 1
+    optional_arguments = 0
+    final_argument_whitespace = True
+    option_spec = {}
 
     def find_key(self, parent):
         if hasattr(parent, 'declared_ibkey'):
@@ -113,8 +123,12 @@ class IBKeyDirective(PyObject):
 
         key = self.find_key(self.content.parent)
 
+        targetnode = nodes.target('', '', ids=[key])
+
         key_elem = keynode(key)
 
+        # Separating argument and content causes two <dd>-s to be generated in
+        # the same <dl>
         doc = addnodes.desc_content()
         update_attrs(doc, self)
         txt = '\n'.join(self.arguments)
@@ -133,21 +147,8 @@ class IBKeyDirective(PyObject):
         source_file = os.path.basename(src_file)
 
         doc_entry = ibkey(self, key, key_elem, doc)
-
-        import inspect as ip
-        print '### {{{'
-        print '\n- '.join(
-            '{0}{1}'.format(item,
-                            '()' if ip.ismethod(tp)
-                            else ' : []' if type(tp) is list
-                            else ' : {}' if type(tp) is dict
-                            else ' : {0}'.format(type(tp)))
-            for item, tp in ((item, getattr(self.state, item))
-                             for item in dir(self.state)))
-        print self.state.parent.parent
-        #print '\n'.join(self.content.parent.parent.parent.parent.parent.parent)
-        print '}}} ###\n'
-
+        #doc_entry = make_admonition(dummy, self.name, 'Alma', self.options, self.content, self.lineno, self.content_offset, self.block_text, self.state, self.state_machine)[0]
+        
         catalog_entry = iblist_entry(
             self,
             env, docname, src_other, source_file, source_line,
@@ -164,7 +165,11 @@ class IBKeyDirective(PyObject):
         env.ibkey_all_ibkeys[key] = dict(docname=docname,
                                          catalog_entry=catalog_entry)
 
-        return [doc_entry]
+        # - super.run() doesnt't kill the structure, it works fine
+        # - returning both superrun and doc_entry does (in any order)
+        # - The todo in the method docstring is rendered outside the 
+        #   method's documentation (?!)
+        return [doc_entry] #[targetnode, doc_entry]
 
 def purge_ibkeys(app, env, docname):
     if not hasattr(env, 'ibkey_all_ibkeys'):
@@ -193,7 +198,7 @@ def setup(app):
     g = globals()
 
     for nodename in ['ibkey', 'keydoc', 'declibkey', 'iblist_entry',
-                     'keynode']:
+                     'keynode', 'dummy']:
         methods = tuple(g['{1}_{0}_node'.format(nodename, role)]
                         for role in ['visit', 'depart'])
         allmethods = dict((k,methods) for k in ['html', 'latex', 'text'])
