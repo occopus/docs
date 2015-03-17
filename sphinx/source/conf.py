41c44d20d9f695e1cf5693278003f4f91d0a908c
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-03-17 12:38:07 +0100

    Change theme to RTD

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -14,6 +14,7 @@
 
 import sys
 import os
+import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -108,7 +109,7 @@ keep_warnings = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+html_theme = 'sphinx_rtd_theme'
 #html_theme = 'default' # Used for debugging the documentation (working and
                        # not-working links are indistinguishable in the nature
                        # theme. But this is not OK for the final output, as
@@ -120,7 +121,7 @@ html_theme = 'nature'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
