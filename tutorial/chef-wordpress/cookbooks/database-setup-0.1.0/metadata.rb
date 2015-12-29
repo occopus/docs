9ed570caaaa23e6c6096b1dd4137d0c5a356a825
Novak Adam <novak.adam@sztaki.mta.hu>
2015-12-29 16:51:47 +0000

    Implement chef-wordpress tutorial

--- tutorial/chef-wordpress/cookbooks/database-setup-0.1.0/metadata.rb
+++ tutorial/chef-wordpress/cookbooks/database-setup-0.1.0/metadata.rb
@@ -2,7 +2,7 @@ name             'database-setup'
 maintainer       'YOUR_COMPANY_NAME'
 maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
-description      'Installs/Configures wordpress-app'
+description      'Installs and configures a mysql database'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends "mysql", ">= 5.0.0"
